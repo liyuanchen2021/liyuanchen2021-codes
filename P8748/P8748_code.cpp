ddfc1fdf654f68a52ea46f5ec9904f7abf789016
luogu557680 <95355326+luogu557680@users.noreply.github.com>
2022-12-27 17:25:39 +0800

    Delete P8748 directory

--- P8748/P8748_code.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-int main() {
-	long long n;
-	scanf("%lld", &n);
-	n /= 1000;	//忽略不足1秒的毫秒数 
-	printf("%02lld:%02lld:%02lld\n", n % 86400 / 3600, n % 3600 / 60, n % 60);
-	return 0;
-}
