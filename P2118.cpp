db0fa7304f4630b3100652d213a885fefde2f8df
liyuanchen2021 <95355326+liyuanchen2021@users.noreply.github.com>
2022-12-31 10:06:45 +0800

    优化 P2118.cpp
    时间复杂度从 O(l^2) 优化为 O(l)

--- P2118.cpp
+++ P2118.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-int gcd(int a, int b) {
-	if (b == 0)	return a;
-	return gcd(b, a % b);
-}
 int main() {
 	int a, b, l;
 	cin >> a >> b >> l;
-	int t = gcd(a, b);
-	a /= t, b /= t;
-	double base = 1.0 * a / b;
-	if (a > l || b > l) {
-		double MIN = 1e9;
-		int A, B;
-		for (int i = 1; i <= l; i++)
-			for (int j = 1; j <= l; j++)
-				if (1.0 * i / j > base && 1.0 * i / j - base < MIN)
-					MIN = 1.0 * i / j - base, A = i, B = j;
-		a = A, b = B;
+	int nowa = 1, nowb = 1, ansa = l, ansb = 1;
+	while (nowa <= l && nowb <= l) {
+		if (nowa * b >= a * nowb) {
+			if (nowa * ansb < ansa * nowb)
+				ansa = nowa, ansb = nowb;
+			nowb++;
+		}
+		else	nowa++;
 	}
-	cout << a << ' ' << b << endl;
+	cout << ansa << ' ' << ansb << endl;
 	return 0;
 }
